c1adb375a865e9524cee1fb9a6637add1abbcf7e
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-08 22:31:45 +0300

    tailwind configuratios done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <div>
-        <header class='text-5xl text-center bg-clip-padding'>
-            <h1>WELCOME!!! <br/>
-                CREATE YOUR OWN BOT ARMY</h1>
+        <header class='text-5xl text-center bg-clip-padding my-3.5'>
+        <h1 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">WELCOME,</span> <br></br>CHOOSE YOUR OWN BOT ARMY!!</h1>
         </header>   
     </div>
   )
