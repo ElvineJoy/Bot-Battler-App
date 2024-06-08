6f1e45b3941183882728a47a4cd6792d8168a83a
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-08 19:34:53 +0300

    printing all bots on page done

--- src/Components/Bot.js
+++ src/Components/Bot.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-function Bot() {
+function Bot({bot}) {
+    
+    
+
+    
   return (
-    <div>Bot</div>
+    <div className='card'>
+        <div className='card-body'>
+            <h1 className='bot-title'>My name is: {bot.name}</h1>
+            <img src={bot.avatar_url} alt={bot.name} />
+            <p className='bot-info'>Damage:{bot.damage}</p>
+            <p className='bot-info'>Bot-Class: {bot.bot_class}</p>
+            <p className='bot-info'>Health: {bot.health}</p>
+            <p className='bot-info'>Catch-phrase: </p>
+
+        </div>
+
+    </div>
   )
 }
 
--- src/Components/BotContainer.js
+++ src/Components/BotContainer.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import Bot from './Bot';
 
 function BotContainer() {
+    const [bots, setBots] = useState([]);
+   
+    // getting the bots from the API using fetch
+    useEffect(() => {
+        fetch("http://localhost:8001/bots")
+        .then(r => r.json())
+        .then(bots => {
+            setBots(bots)}) 
+    }, []);
+    // console.log(bots) 
+
+    const botsList = bots.map((bot) =>(
+        <Bot 
+        key = {bot.id}
+        bot = {bot}
+        />
+    ))
+
+    
+
   return (
-    <div>BotContainer</div>
+    <div>
+        {botsList}
+    </div>
   )
 }
 
