a667c73c5e37e97db50eef2d7a635fec3ed45713
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-09 21:40:59 +0300

    ReadMe Updated

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # BOT BATTLER APP
+
+## About
+Are you a Bot fanatic, do you wish to form your own Bot Army? I have something for you!!!... This App lets you do just that.
+
+Here you can click on the bot to join your army, you can click on the bot to remove it from your army too.
+Best yet, you want to retire the bot forever? After giving you service you can have it removed from the collection all together. 
+
+You can filter and sort the bots too. Try it out!!
+
+## The App Outlook
+What should you expect?
+![Alt text](public/Bot Battler App.PNG)
+
+### Prerequisites
+- Node.js
+- npm
+- json server
+    - Run json-server --watch db.json --port 8001
+
+## Available Scripts
+** npm start**
+This runs the app in the development mode.
+Open http://localhost:3000 and view it in your browser.
+
+## Built with
+- React
+- Tailwind css
+
+## License 
+MIT License Copyright (c) 2024 Joy_W
--- src/Components/YourBotArmy.js
+++ src/Components/YourBotArmy.js
@@ -16,7 +16,7 @@ return (
   <div className="container mx-auto p-3">
     <div className="w-full bg-white border border-red-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <h1 className="text-center text-5xl font-extrabold dark:text-white">YOUR BOT ARMY CONSISTS OF ...</h1>
-        <h6 className=" p-5 text-center text-base font-extrabold text-gray-900 dark:text-white"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Instructions: choose your desired bots from our collection below and make your own army as desired.</span></h6>
+        <h6 className=" p-5 text-center text-base font-extrabold text-gray-900 dark:text-white"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Instructions: Choose your desired bots from our collection below and make your own army as desired.</span></h6>
         <div className=" my-11 ml-4 mr-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         
         {myArmylist}
